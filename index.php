bbc7153156fd4942b774fae838d61f6d59235dfc
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-03-20 11:45:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require('class/News.php');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+	<link rel="shortcut icon" href="images/favicon.png">
     <title>NAMDEVEL - Informasi Berita Terupdate Hari Ini</title>
   </head>
   <body style="background-color:rgb(247, 247, 247);">
